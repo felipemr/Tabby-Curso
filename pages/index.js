b745df5817f10fc4871655c624edb1734bdcb42a
Felipe Marques <felipemr94@outlook.com>
2023-11-22 15:48:03 +0000

    atualizando mensagem inicial.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <div><h1>Oi Mozi!</h1><p>Te amo minha lindona S2 </p></div>
+    return <div><h1>Oi Mozi!</h1><p>Te amo minha lindona S2 </p><p>Tenho certeza que a coleção da loja será um sucesso enorme! <strong>Você arrasa muito!</strong></p></div>
 }
 
 export default Home;
